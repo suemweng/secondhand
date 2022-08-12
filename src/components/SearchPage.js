8493c6fb9c6692f03df71dbfabf713305bf05c6a
Sue <suemweng@gmail.com>
2022-08-12 16:32:48 -0400

    To move test data to utils.js

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Button, Col, Layout, Menu, message, Row, Tooltip, List, Card } from 'antd';
-
-import item1 from '../assets/images/item1.webp';
-import item2 from '../assets/images/item2.jpeg';
-import item3 from '../assets/images/item3.jpeg';
 import { getAllItems, searchItemsByGenreId } from "../utils";
 
 
@@ -19,36 +15,16 @@ const menuItem = [
   function SearchPage ({list, onSuccess}) {
 
     const [loading, setLoading] = useState(false);
-    // const [data, setData] = useState([]);
 
-    const dataAll = [
-      {
-        itemId: 1,
-        genreId: 'Shoes',
-        title:'item 1',
-        image: item1
-      },
-      {
-        itemId: 2,
-        genreId: 'Bags',
-        title:'item 2',
-        image: item2
-      },
-      {
-        itemId: 3,
-        genreId: 'Clothes',
-        title:'item 3',
-        image: item3
-      }
-    ]
-
+    // DidMount to getAllItems
     useEffect(() =>{
       setLoading(true);
 
       try {
         //const resp = await getAllItems();
-        // onSuccess(resp);
-        onSuccess(dataAll);
+        const resp = getAllItems();
+        onSuccess(resp);
+        //onSuccess(dataAll);
 
       } catch (error) {
         message.error(error.message);
@@ -78,8 +54,8 @@ const menuItem = [
 
     try {
       //const resp = await searchItemsByGenreId(key);
-      // setData(resp);
-      const resp = dataAll.filter((item) => item.genreId === key);
+      const resp = searchItemsByGenreId(key);
+      //const resp = dataAll.filter((item) => item.genreId === key);
       onSuccess(resp);
       message.info(`Category selected: ${key}`);
     } catch (error) {
@@ -110,9 +86,9 @@ const menuItem = [
             grid={{
                 xs: 1,
                 sm: 2,
-                md: 4,
-                lg: 4,
-                xl: 6,
+                md: 2,
+                lg: 2,
+                xl: 3,
             }}
             dataSource={list}
             renderItem={item => (
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,30 @@
+import item1 from './assets/images/item1.webp';
+import item2 from './assets/images/item2.jpeg';
+import item3 from './assets/images/item3.jpeg';
+
+ // temperay fake data
+ const dataAll = [
+    {
+      itemId: 1,
+      genreId: 'Shoes',
+      title:'item 1',
+      image: item1
+    },
+    {
+      itemId: 2,
+      genreId: 'Bags',
+      title:'item 2',
+      image: item2
+    },
+    {
+      itemId: 3,
+      genreId: 'Clothes',
+      title:'item 3',
+      image: item3
+    }
+  ]
+
+
 const domain = "";
 const allItemsUrl = `${domain}/product`;
 const searchItemsByGenreIdUrl = `${domain}/product/genre_id=`;
@@ -8,20 +35,24 @@ export const login = (Credential) => { };
 export const register = (Credential) => { };
 
 export const getAllItems = () => {
-    return fetch(allItemsUrl).then((response) => {
-        if (response.status !== 200) {
-            throw Error('Fail to get all items');
-        }
+    return dataAll;
+    // return fetch(allItemsUrl).then((response) => {
+    //     if (response.status !== 200) {
+    //         throw Error('Fail to get all items');
+    //     }
 
-        return response.json();
-    })
+    //     return response.json();
+    // })
 }
 
-export const searchItemsByGenreId = (genreId) => {
-    return fetch(`${searchItemsByGenreIdUrl}${genreId}`).then((response) => {
-        if (response.status !== 200) {
-            throw Error('Fail to find the genre');
-        }
-        return response.json();
-    })
+export const searchItemsByGenreId = (genreIdSelected) => {
+
+    return dataAll.filter((item) => item.genreId === genreIdSelected);
+
+    // return fetch(`${searchItemsByGenreIdUrl}${genreId}`).then((response) => {
+    //     if (response.status !== 200) {
+    //         throw Error('Fail to find the genre');
+    //     }
+    //     return response.json();
+    // })
 }
