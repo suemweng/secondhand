3af948a8098838cdcd9a1ea44bed855c0a066856
tjtse2000 <95405318+tjtse2000@users.noreply.github.com>
2022-08-16 22:20:39 -0400

    AccountInfo

--- src/components/AcctInfo.js
+++ src/components/AcctInfo.js
@@ -1,12 +1,58 @@
+import { Button, Form, message } from "antd";
 import React from "react";
+import ItemUpload from "./ItemUpload";
 
+class AcctInfo extends React.Component {
+    formRef = React.createRef();
+    state = {loading: false,};
 
-function AcctInfo () {
+    onFinish = () => {
+        console.log("Completed!");
+    };
 
+    onClick = () => {
+        return ItemUpload;
+    }
 
-    return (
-        <div style={{fontSize:50, width:'100%', textAlign:'center'}}> Account Info </div>
-    )
+    render() {
+        return (
+            <div style={{width: 1500, margin: "auto"}}>
+                <Form
+                    name="Account Information" 
+                    onFinish = {this.onFinish} 
+                    style={{margin: "auto"}}
+                >
+                    <Form.Item name="Username:" rules={[{required: true,}]}>
+                    </Form.Item>
+                    <Form.Item name="Alias:" rules={[{required: true,}]}>
+                    </Form.Item>
+                    <Form.Item name="Location:" rules={[{required: true,}]}>
+                    </Form.Item>
+                </Form>
+                <Form 
+                    name="Uploaded Items" 
+                    ref = {this.formRef} 
+                    onFinish = {this.onFinish}
+                    style={{margin: "auto"}}
+                >
+                    <Form.Item name="Item1:" rules={[{required: true,}]}>
+                    </Form.Item>
+                    <Form.Item name="Item2:" rules={[{required: true,}]}>
+                    </Form.Item>
+                    <Form.Item name="Item3:" rules={[{required: true,}]}>
+                    </Form.Item>
+                </Form>
+            <Button
+                onClick={this.onClick}
+                disabled={this.state.loading}
+                shape="default"
+                type="primary"
+            >
+                Add Item
+            </Button>
+            </div>
+        );
+    }
 }
 
 export default AcctInfo;
